1109aafec75b071b962d21609a2ed283e6301e26
pak <pak@1916.lan>
2019-01-24 12:44:07 -0500

    add section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react'
+import SectionView from './view/SectionView';
+import uuidv4 from 'uuid/v4';
 
 export default class Section extends Component {
+
+  state = {
+   userInfo: [
+    {
+      name: "Hamster overlord",
+      title: "Chief Executive Officer",
+      summary: "Est sed tellus nunc hendrerit adipiscing nulla nunc id elit etiam tempus lectus vel posuere congue nunc quam varius lectus.",
+      id: uuidv4()
+    }
+   ]
+  }
+
+
   render() {
+
+
     return (
       
 
@@ -16,57 +33,17 @@ export default class Section extends Component {
         </form>
           </div>
           <div className="group excerpts">
-            <article className="one_third first">
-              <div className="hgroup">
-                <h6 className="heading">A. Doe</h6>
-                <em>Chief Executive Officer</em></div>
-              <figure><a href="#"><img src="/demo/320x220.png" alt="true" /></a></figure>
-              <div className="txtwrap">
-                <p>Est sed tellus nunc hendrerit adipiscing nulla nunc id elit etiam tempus lectus vel posuere congue nunc quam varius lectus.</p>
-              </div>
-              <footer>
-                <ul className="faico clear">
-                  <li><a className="faicon-twitter" href="#"><i className="fa fa-twitter" /></a></li>
-                  <li><a className="faicon-linkedin" href="#"><i className="fa fa-linkedin" /></a></li>
-                  <li><a className="faicon-google-plus" href="#"><i className="fa fa-google-plus" /></a></li>
-                  <li><a className="faicon-vk" href="#"><i className="fa fa-vk" /></a></li>
-                </ul>
-              </footer>
-            </article>
-            <article className="one_third">
-              <div className="hgroup">
-                <h6 className="heading">B. Doe</h6>
-                <em>General Manager</em></div>
-              <figure><a href="#"><img src="/demo/320x220.png" alt="true" /></a></figure>
-              <div className="txtwrap">
-                <p>At rutrum diam sem sed turpis mauris pede arcu molestie ut scelerisque id tincidunt tincidunt neque proin iaculis mauris eu.</p>
-              </div>
-              <footer>
-                <ul className="faico clear">
-                  <li><a className="faicon-twitter" href="#"><i className="fa fa-twitter" /></a></li>
-                  <li><a className="faicon-linkedin" href="#"><i className="fa fa-linkedin" /></a></li>
-                  <li><a className="faicon-google-plus" href="#"><i className="fa fa-google-plus" /></a></li>
-                  <li><a className="faicon-vk" href="#"><i className="fa fa-vk" /></a></li>
-                </ul>
-              </footer>
-            </article>
-            <article className="one_third">
-              <div className="hgroup">
-                <h6 className="heading">C. Doe</h6>
-                <em>Marketing Manager</em></div>
-              <figure><a href="#"><img src="/demo/320x220.png" alt="true" /></a></figure>
-              <div className="txtwrap">
-                <p>Sodales cursus sapien erat pharetra justo vitae volutpat eros magna et magna praesent at lectus vestibulum ante ipsum primis.</p>
-              </div>
-              <footer>
-                <ul className="faico clear">
-                  <li><a className="faicon-twitter" href="#"><i className="fa fa-twitter" /></a></li>
-                  <li><a className="faicon-linkedin" href="#"><i className="fa fa-linkedin" /></a></li>
-                  <li><a className="faicon-google-plus" href="#"><i className="fa fa-google-plus" /></a></li>
-                  <li><a className="faicon-vk" href="#"><i className="fa fa-vk" /></a></li>
-                </ul>
-              </footer>
-            </article>
+            {
+              this.state.userInfo.map((user) => {
+                return (
+                  <SectionView 
+                    name={user.name}
+                    title={user.title}
+                    summary={user.summary}
+                  />
+                )
+              })
+            }
           </div>
           <div className="clear" />
         </main>
---
+++ src/components/view/SectionView.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+function SectionView (props) {
+    return (
+        <div>
+            <article className="one_third first">
+              <div className="hgroup">
+                <h6 className="heading">{props.name}</h6>
+                <em>{props.title}</em></div>
+              <figure><a href="#"><img src="/demo/320x220.png" alt="true" /></a></figure>
+              <div className="txtwrap">
+                <p>{props.summary}</p>
+              </div>
+            </article>
+        </div>
+    )
+}
+
+export default SectionView;
